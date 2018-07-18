97780c1264d10eda6970b214711f8be29386aaa0
Alopex6414 <1029535012@qq.com>
2018-07-19 00:23:24 +0800

    Assistant Add Bin.

---
+++ Debug/script/AssistantMain.lua
@@ -0,0 +1,39 @@
+--------------------------------------------------------------
+-- AssistantMain Scene Lua Script
+-- @brief AssistantMain Scene Script Config Parameters
+-- @author alopex
+-- @version v1.00a
+-- @date 2018-07-15
+--------------------------------------------------------------
+
+-----------------------
+--AssistantMain Scene
+-----------------------
+
+--AssistantMain BackGround背景
+MainBackGroundWidth=960         --AssistantMain背景宽度
+MainBackGroundHeight=720        --AssistantMain背景高度
+MainBackTextureWidth=1024       --AssistantMain背景纹理宽度
+MainBackTextureHeight=1024      --AssistantMain背景纹理高度
+
+--AssistantMain Menu菜单
+MainBackMenuWidth=160           --AssistantMain菜单宽度
+MainBackMenuHeight=720          --AssistantMain菜单高度
+MainBackMenuTextureWidth=256    --AssistantMain菜单纹理宽度
+MainBackMenuTextureHeight=1024  --AssistantMain菜单纹理高度
+
+--AssistantMain Logo图标
+MainLogPosX=0                   --AssistantMain图标坐标X
+MainLogPosY=0                   --AssistantMain图标坐标Y
+MainLogoWidth=120               --AssistantMain图标宽度
+MainLogoHeight=22               --AssistantMain图标高度
+MainLogoTextureWidth=256        --AssistantMain图标纹理宽度
+MainLogoTextureHeight=256       --AssistantMain图标纹理高度
+
+--AssistantMain Apps字体
+MainAppPosX=0                   --AssistantMain应用坐标X
+MainAppPosY=0                   --AssistantMain应用坐标Y
+MainAppWidth=34                 --AssistantMain应用宽度
+MainAppHeight=11                --AssistantMain应用高度
+MainAppTextureWidth=256         --AssistantMain应用纹理宽度
+MainAppTextureHeight=256        --AssistantMain应用纹理高度
